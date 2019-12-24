b7eba74e61c5840807ac8a795a9741459eb854e8
Devin Riegle <riegledevin@gmail.com>
2019-12-23 22:13:10 -0500

    add initial plaid token exchange endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const plaid = require("plaid");
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.load();
+
+function handleError(errorMessage) {
+  console.error(errorMessage);
+}
 
 const client = new plaid.Client(
   process.env.PLAID_CLIENT_ID,
@@ -11,10 +18,28 @@ const client = new plaid.Client(
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello, world"
+  });
+});
+
+app.post("/plaid_token_exchange", async (req, res) => {
+  const { publicToken } = req.body;
+
+  const { access_token } = await client
+    .exchangePublicToken(publicToken)
+    .catch(handleError);
+
+  const { accounts, item } = await client
+    .getAccounts(access_token)
+    .catch(handleError);
+
+  console.log({
+    accounts,
+    item
   });
 });
 
